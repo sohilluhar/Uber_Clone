1b7724537a90166f9d23c37c6f9def52ae5940f1
sohilluhar <sohilayubluhar@gmail.com>
2019-10-25 21:14:50 +0530

    Home Page Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -15,6 +16,8 @@ class _MyAppState extends State<MyApp> {
   final Set<Marker> _markers = new Set();
   static const LatLng _mainLocation = const LatLng(19.0737446, 72.8994785);
 
+//  static var screensize = size;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,153 +25,274 @@ class _MyAppState extends State<MyApp> {
           primaryColor: Colors.black,
         ),
         home: Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: _mainLocation,
-                zoom: 15.0,
-              ),
-              markers: this.myMarker(),
-              mapType: MapType.normal,
-              onMapCreated: (controller) {
-                setState(() {
-                  myMapController = controller;
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(),
+          body: Stack(
+//
+              children: <Widget>[
+                GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: _mainLocation,
+                    zoom: 15.0,
+                  ),
+                  markers: this.myMarker(),
+                  mapType: MapType.normal,
+                  onMapCreated: (controller) {
+                    setState(() {
+                      myMapController = controller;
+                    });
+                  },
+                ),
+                Container(
+//                  margin: EdgeInsets.only(top: 80, right: 10),
+                  alignment: Alignment.bottomCenter,
 
-                });
-              },
-
-
-            ),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            Container(
-              height: 250.0,
-              child: DrawerHeader(
-                child: new Column(
-                  children: <Widget>[
-                    new Row(
+                  child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: <Widget>[
-                        new Container(
-                          child: new CircleAvatar(
-                            backgroundImage: new NetworkImage(
-                                'https://i.ibb.co/jDvLWGV/user.png'),
-                            backgroundColor: Colors.white,
-                            radius: 30.0,
-                          ),
+                        new Row(children: <Widget>[
+                          new Container(
+                              height: 60.0,
+                              width: 360.0,
+                              color: Colors.white,
+                              child: Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                                  child: new Text("Good evening , Sohil",
+                                      textAlign: TextAlign.center,
+                                      style: new TextStyle(
+                                          color: Colors.black,
+                                          backgroundColor: Colors.white,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold))))
+                        ]),
+                        new Row(children: <Widget>[
+                          new Container(
+                              color: Colors.white,
+                              height: 60.0,
+                              width: 225.0,
+                              child: Padding(
+                                  padding: EdgeInsets.fromLTRB(20, 0, 0, 15),
+                                  child: RaisedButton(
+                                    color: Colors.black12,
+                                    child: Text(
+                                      "Where to  ?",
+                                      textAlign: TextAlign.center,
+                                      style: new TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                  ))),
+                          new Container(
+                              color: Colors.white,
+                              height: 60.0,
+                              width: 135.0,
+                              child: Padding(
+                                  padding: EdgeInsets.fromLTRB(5, 0, 20, 15),
+                                  child: RaisedButton(
+                                    color: Colors.black12,
+                                    child: Text(
+                                      "Schedule",
+                                      textAlign: TextAlign.center,
+                                      style: new TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                  ))),
+                        ]),
+                        new Row(
+                          children: <Widget>[
+                            new Container(
+                                alignment: Alignment.bottomLeft,
+                                color: Colors.white,
+                                height: 60.0,
+                                width: 360.0,
+                                child: Padding(
+                                    padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
+                                    child: FlatButton.icon(
+                                        onPressed: null,
+                                        icon: Icon(Icons.stars),
+                                        label: Text(
+                                          "Choose a saved place",
+                                          textAlign: TextAlign.start,
+                                          style: new TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 18.0,
+                                              fontWeight: FontWeight.bold),
+                                        ))))
+                          ],
                         ),
-                        new Container(
-                          child: new Text(
-                            "    Sohil Luhar",
-                            style: new TextStyle(
-                                color: Colors.white, fontSize: 20),
-                          ),
+                        new Row(
+                          children: <Widget>[
+                            new Container(
+                              color: Colors.white,
+                              height: 60.0,
+                              width: 180.0,
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(20, 0, 0, 15),
+                                child: FlatButton.icon(
+                                    onPressed: null,
+                                    icon: Icon(Icons.local_taxi),
+                                    label: Text(
+                                      "Ride",
+                                      textAlign: TextAlign.start,
+                                      style: new TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                              ),
+                            ),
+                            new Container(
+                              color: Colors.white,
+                              height: 60.0,
+                              width: 180.0,
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(20, 0, 10, 20),
+                                child: FlatButton.icon(
+                                    onPressed: null,
+                                    icon: Icon(Icons.restaurant),
+                                    label: Text(
+                                      "Order Food",
+                                      textAlign: TextAlign.start,
+                                      style: new TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                              ),
+                            )
+                          ],
                         )
-                      ],
-                    ),
-                    new Row(
+                      ]),
+                ),
+              ]),
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                Container(
+                  height: 250.0,
+                  child: DrawerHeader(
+                    child: new Column(
                       children: <Widget>[
-                        new Container(
-                          padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
-                        )
+                        new Row(
+                          children: <Widget>[
+                            new Container(
+                              child: new CircleAvatar(
+                                backgroundImage: new NetworkImage(
+                                    'https://i.ibb.co/jDvLWGV/user.png'),
+                                backgroundColor: Colors.white,
+                                radius: 30.0,
+                              ),
+                            ),
+                            new Container(
+                              child: new Text(
+                                "    Sohil Luhar",
+                                style: new TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            )
+                          ],
+                        ),
+                        new Row(
+                          children: <Widget>[
+                            new Container(
+                              padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
+                            )
+                          ],
+                        ),
+                        new Row(children: <Widget>[
+                          new Container(
+                              child: Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                                  child: new Text("Do more with your account",
+                                      style: new TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 14.0))))
+                        ]),
+                        new Row(children: <Widget>[
+                          new Container(
+                              child: Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                                  child: new Text("Make money driving",
+                                      style: new TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 14.0))))
+                        ])
                       ],
                     ),
-                    new Row(children: <Widget>[
-                      new Container(
-                          child: Padding(
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                              child: new Text("Do more with your account",
-                                  style: new TextStyle(
-                                      color: Colors.white, fontSize: 14.0))))
-                    ]),
-                    new Row(children: <Widget>[
-                      new Container(
-                          child: Padding(
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                              child: new Text("Make money driving",
-                                  style: new TextStyle(
-                                      color: Colors.white, fontSize: 14.0))))
-                    ])
-                  ],
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
-                decoration: BoxDecoration(
-                  color: Colors.black,
+                ListTile(
+                  title: Text(
+                    'Your Trips',
+                    style: new TextStyle(fontSize: 22),
+                  ),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
                 ),
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Your Trips',
-                style: new TextStyle(fontSize: 22),
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Help',
-                style: new TextStyle(fontSize: 22),
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Payment',
-                style: new TextStyle(fontSize: 22),
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Free Rides',
-                style: new TextStyle(fontSize: 22),
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Settings',
-                style: new TextStyle(fontSize: 22),
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
+                ListTile(
+                  title: Text(
+                    'Help',
+                    style: new TextStyle(fontSize: 22),
+                  ),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Payment',
+                    style: new TextStyle(fontSize: 22),
+                  ),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Free Rides',
+                    style: new TextStyle(fontSize: 22),
+                  ),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Settings',
+                    style: new TextStyle(fontSize: 22),
+                  ),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
 //            Expanded(
 //              child: new Align(
 //                alignment: Alignment.bottomRight,
@@ -178,13 +302,11 @@ class _MyAppState extends State<MyApp> {
 //                ,
 //              ),
 //            )
-          ],
-        ),
-      ),
-    ));
+              ],
+            ),
+          ),
+        ));
   }
-
-
 
   Set<Marker> myMarker() {
     setState(() {
@@ -192,7 +314,7 @@ class _MyAppState extends State<MyApp> {
         // This marker id can be anything that uniquely identifies each marker.
         markerId: MarkerId(_mainLocation.toString()),
         position: _mainLocation,
-        icon: BitmapDescriptor.defaultMarker,//(,"assets/images/taxi.png"),
+        icon: BitmapDescriptor.defaultMarker, //(,"assets/images/taxi.png"),
       ));
     });
 
